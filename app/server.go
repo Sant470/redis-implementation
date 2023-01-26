f189a51cd9c4b83356a38393fd5650cbf07e8d21
Sant Purushotam <sant.purushotam@thefastway.in>
2023-01-26 20:06:18 +0530

    refactor 5th stage

--- app/server.go
+++ app/server.go
@@ -1,88 +1,125 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
 const (
+	// Decoding ...
+	ARRAY byte = '*'
+	BULK_STRING byte = '$'
+	STRING byte = '+'
+
+	// Encoding ...
 	STRING_PREFIX = "+"
-	CRLF = "\r\n"
+	CRLF          = "\r\n"
 )
 
-type commandDetails struct {
-	command string
-	count int 
-	data interface{}
+func encode(res string) string {
+	return fmt.Sprintf("%s%s%s", STRING_PREFIX, res, CRLF)
 }
 
-func respConversion(res, category string) string {
-	switch category {
-	case "string":
-		return STRING_PREFIX+res+CRLF
+func decode(r *bufio.Reader) ([]string, error) {
+	b, err := r.ReadByte()
+	if err != nil {
+		return nil, err  
+	}
+	switch b {
+	case ARRAY:
+		return decodeArray(r)
+	case STRING:
+		return decodeString(r)
+	case BULK_STRING:
+		return decodeBulkString(r)
 	default:
-		return STRING_PREFIX+res+CRLF
+		return nil, fmt.Errorf("could not decode the stream")
 	}
 }
 
-func decodeArray(buf string) []string {
-	cmds := []string{}
-	arr := strings.Split(buf, CRLF)
-	for _, ele := range arr {
-		if strings.HasPrefix(ele, "*") || strings.HasPrefix(ele, "$"){
-			continue
-		} else {
-			cmds = append(cmds, ele)
+func decodeString(r *bufio.Reader) ([]string, error) {
+	barr, err := validByteCount(r)
+	if err != nil {
+		return nil, err
+	}
+	return []string{string(barr)}, nil 
+}
+
+func decodeBulkString(r *bufio.Reader) ([]string, error) {
+	barr, err := validByteCount(r)
+	if err != nil {
+		return nil, err 
+	}
+	count, err:= strconv.Atoi(string(barr))
+	if err != nil {
+		return nil, err 
+	}
+	buf := make([]byte, count+2)
+	_, err = io.ReadFull(r, buf)
+	if err != nil && err != io.EOF {
+		return nil, err 
+	}
+	return []string{string(buf)}, nil 
+}
+
+func decodeArray(r *bufio.Reader) ([]string, error) {
+	byteCounts, err := validByteCount(r)
+	if err != nil {
+		return nil, err
+	}
+	count, err:= strconv.Atoi(string(byteCounts))
+	if err != nil {
+		return nil, err 
+	}
+	vals := []string{}
+	for i:=0; i<count; i++ {
+		current, err := decode(r)
+		if err == io.EOF {
+			return vals, nil
 		}
+		if err != nil {
+			return nil, err 
+		}
+		vals = append(vals, current...)
 	}
-	return cmds
+	return vals, nil  
 }
 
-func decodeResp(buf string) ([]string) {
-	cmds := []string{}
-	if strings.HasPrefix(buf, "*") {
-		cmds = decodeArray(buf)
+func validByteCount(r *bufio.Reader) ([]byte, error) {
+	barr, err := r.ReadBytes('\n')
+	if err != nil {
+		return nil,err
 	}
-	return cmds
+	return barr[:len(barr)-2], nil 
 }
-
-
-
 
 func handleConn(conn net.Conn) {
 	defer conn.Close()
-	req := make([]byte, 64)
 	for {
-		_, err := conn.Read(req)
-		if err == io.EOF {
-			break
-		}
+		r := bufio.NewReader(conn)
+		cmds, err := decode(r)
 		if err != nil {
-			fmt.Println("error reading from incoming stream", err)
-			break
+			conn.Write([]byte(encode("invaid request")))
 		}
-		cmds := decodeResp(string(bytes.TrimSpace(req)))
-		if len(cmds) >=1 {
-			switch strings.ToUpper(cmds[0]) {
-			case "PING":
-				conn.Write([]byte(respConversion("PONG", "string")))
-			case "ECHO":
-				conn.Write([]byte(respConversion(cmds[1], "string")))
+		if len(cmds) >= 1 {
+			cmd := strings.ToUpper(cmds[0])
+			fmt.Printf("fucking command: %s type :%T", cmd, cmd)
+			switch  {
+			case cmd == "PING":
+				conn.Write([]byte(encode("PONG")))
+			case cmd == "ECHO":
+				conn.Write([]byte(encode(cmds[1])))
 			}
-		}
-		if err != nil {
-			fmt.Println("error parsing resp")
 		}
 	}
 }
 
-
-
-func main() {	
+func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379", err)
@@ -99,6 +136,5 @@ func main() {
 	}
 
 }
-
 
 // *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
